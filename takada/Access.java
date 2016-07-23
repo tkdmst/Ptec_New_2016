2153ce46059a3035a629d959c5e822d3a3598cf6
tkdmst <takada.misato@p-tec.jp>
2016-07-23 13:39:27 +0900

    Update Access.java
    独自の例外(AccessException)へ変更しました。

--- takada/Access.java
+++ takada/Access.java
@@ -2,11 +2,11 @@ package socDB;
 
 public interface Access {
 
-	public void initialize() throws Exception;
+	public void initialize() throws AccessException;
 
-	public void write(Model model) throws Exception;
+	public void write(Model model) throws AccessException;
 
-	public Model read() throws Exception;
+	public Model read() throws AccessException;
 
 	public void close();
 
