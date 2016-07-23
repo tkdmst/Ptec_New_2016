fe4f4b74ac23f6889af426cf5e4b0f569325014d
tkdmst <takada.misato@p-tec.jp>
2016-07-23 13:44:36 +0900

    Update Controller.java
    独自例外に変更しました

--- takada/Controller.java
+++ takada/Controller.java
@@ -1,32 +1,31 @@
 package socDB;
 
-import java.sql.SQLException;
-
 public class Controller {
 	private final View view;
-	private final ClientDB cDB;
+	private final Access ac;
 
 	// コンストラクタ
-	Controller(View view, ClientDB cDB) {
+	Controller(View view, Access ac) {
 		this.view = view;
-		this.cDB = cDB;
+		this.ac = ac;
 	}
 
 	// クライアントの主な動き方 入力、書き込み、読み込み、表示
-	public void proc() throws Exception {
+	public void proc() throws AccessException {
 		while (true) {
 			try {
 				System.out.println(">開始 入力で新規 stopで終了");
 				Model model = view.input();
 				if (model.toString().equals("stop"))
 					break;
-				cDB.write(model);
+				ac.write(model);
 				System.out.println(">write 終了");
-				model = cDB.read();
+				model = ac.read();
 				view.display(model);
 				System.out.println(">read終了");
-			} catch (SQLException e) {
-				throw new Exception("proc内の致命的なエラー");
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+				throw new AccessException("proc内の致命的なエラー");
 			}
 		}
 	}
