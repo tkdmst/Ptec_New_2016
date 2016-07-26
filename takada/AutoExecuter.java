8850fdd0bec9ac7af0bbc8c43f3f688f929cd973
tkdmst <takada.misato@p-tec.jp>
2016-07-26 10:58:00 +0900

    Create AutoExecuter.java
    3秒ごとに"小休止3秒"とテーブルに打たせる構造です。

---
+++ takada/AutoExecuter.java
@@ -0,0 +1,22 @@
+package socDB;
+
+public class AutoExecuter implements Runnable {
+	private boolean runner = true;
+	public void run(){
+		Access db = ClientDB.instance();
+		try {
+			while (runner) {
+				db.write(new Model("小休止3秒"));
+				Thread.sleep(3000);
+			}
+		} catch (AccessException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void threadEnd() {
+		runner = false;
+	}
+}
