4e25124f77073b68aec7d725766ebf840614463f
tkdmst <takada.misato@p-tec.jp>
2016-07-27 17:12:02 +0900

    Update ClientDB.java
    Javadocへの変更と、synchronizedの導入をしました。
    全てのファイルでコメントを入れ替えましたが、Windows版がよくわからずです。
    ブラウザ版であげさせて頂きました。

--- takada/ClientDB.java
+++ takada/ClientDB.java
@@ -7,6 +7,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * ClientDBクラスはこのプログラムの主な処理を担当しています。<br>
+ * Accessの行き着く先です。 ※関連項目参照<br>
+ * Singletonにしてあることで簡単にはインスタンス化できなくなっています。
+ *
+ * @see Access
+ */
 public class ClientDB implements Access {
 	private Connection con;
 	private PreparedStatement write_pstmt;
@@ -15,22 +22,29 @@ public class ClientDB implements Access {
 	private int nom = 1;
 	private String sql;
 
-	// singletonパターンで
+	// singletonパターンに必須
 	private static ClientDB clidb;
 
-	// instance()のためにprivateでインスタンス化を不可に
+	/** コンストラクタではインスタンス化できないようになっています。 */
 	private ClientDB() {
 		clidb = null;
 	}
 
-	// instance()を呼び出したときだけインスタンス化できる
+	/**
+	 * instanceメソッドを呼び出したときだけインスタンス化可能になります。
+	 *
+	 * @return clidb インスタンス化
+	 */
 	public static ClientDB instance() {
 		if (clidb == null)
 			clidb = new ClientDB();
 		return clidb;
 	}
 
-	// 初期化 ドライバーとの接続
+	/**
+	 * initializeは初期化を行います。<br>
+	 * ドライバーとの接続、プリペアの作成、ステートメントの作成、オートコミットの停止など
+	 */
 	public void initialize() throws AccessException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -47,20 +61,31 @@ public class ClientDB implements Access {
 		}
 	}
 
-	// 書き込み
+	/**
+	 * writeはDBに書き込みをします。<br>
+	 * 書き込む手段として、PKの番号は小さい順に総当りで探し、その番号をロックしてから書き込みを行います。<br>
+	 * コミットもこのメソッドで行います。失敗時にはロールバックをします。<br>
+	 * 空いているレコードを探す処理からロックまでをsynchronizedにしました。
+	 *
+	 * @param model
+	 *            Modelクラスの利用
+	 * @throws AccessException
+	 *             エラー時にメッセージをつけて処理
+	 */
 	public void write(Model model) throws AccessException {
 		try {
 			boolean flag = false;
-			while (true) {
-				sql = "exists(Select* from socDB where nom=" + nom + ")";
-				rset = stmt.executeQuery("Select " + sql);
-				while (rset.next()) {
-					if (rset.getString(sql).equals("0"))
+			synchronized (this) {
+				while (true) {
+					sql = "Select* from socDB where nom=" + nom;
+					rset = stmt.executeQuery(sql);
+					if (!rset.next())
 						flag = true;
+					if (flag)
+						break;
+					nom++;
 				}
-				if (flag)
-					break;
-				nom++;
+				stmt.executeQuery(sql + " for update");
 			}
 			write_pstmt.setInt(1, nom);
 			write_pstmt.setString(2, model.toString());
@@ -68,8 +93,9 @@ public class ClientDB implements Access {
 			con.commit();
 		} catch (SQLException e) {
 			try {
+				e.printStackTrace();
 				con.rollback();
-			} catch (Exception e1) {
+			} catch (SQLException e1) {
 				throw new AccessException("ロールバックエラー");
 			}
 			e.printStackTrace();
@@ -77,7 +103,12 @@ public class ClientDB implements Access {
 		}
 	}
 
-	// 読み込み
+	/**
+	 * readは最新更新日のレコードの読み込みをします。<br>
+	 * 読み込んだデータはModelクラスの形で帰します。<br>
+	 *
+	 * @return Model(sql) 得たsqlをModelクラスに引き渡す
+	 */
 	public Model read() throws AccessException {
 		sql = "Select* from socDB where upd_date=(Select Max(upd_date) from socDB)";
 		try {
@@ -91,7 +122,7 @@ public class ClientDB implements Access {
 		return new Model(sql);
 	}
 
-	// クローズ
+	/** closeはクローズ処理を行います。 */
 	public void close() {
 		try {
 			if (con != null)
