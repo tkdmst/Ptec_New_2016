a29edd33e60b32a690758c413d2f921515447212
tkdmst <takada.misato@p-tec.jp>
2016-07-28 16:54:29 +0900

    synchronizedの実装まで
    Javadocの表示で大きく変更しましたのでアップロードいたします。
    よろしくお願いします。
    現状synchronizedの実装までになっています。

--- takada/ClientDB.java
+++ takada/ClientDB.java
@@ -7,42 +7,83 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class ClientDB {
+/**
+ * ClientDBクラスはこのプログラムの主な処理を担当しています。<br>
+ * Accessの行き着く先です。 ※関連項目参照<br>
+ * Singletonにしてあるため、簡単にはインスタンス化できなくなっています。
+ *
+ * @see Access
+ */
+public class ClientDB implements Access {
 	private Connection con;
 	private PreparedStatement write_pstmt;
-	private Statement stmt;
-	private ResultSet rset;
-	private int nom = 1;
-	private String sql;
+	private Statement stmt, stmtRead;
+	private ResultSet rset, rsetRead;
+	private int nom;
+	private String sql, sqlRead;
+	// singletonパターン
+	private static ClientDB clidb;
+	// private Object lock = new Object();
 
-	// 初期化 ドライバーとの接続
-	public void initialize() throws Exception {
+	/** コンストラクタではインスタンス化できないようになっています。 */
+	private ClientDB() {
+		clidb = null;
+	}
+
+	/**
+	 * instanceメソッドを呼び出したときだけインスタンス化可能になります。
+	 *
+	 * @return インスタンス化を可能にする
+	 */
+	public static ClientDB instance() {
+		if (clidb == null)
+			clidb = new ClientDB();
+		return clidb;
+	}
+
+	/**
+	 * initializeは初期化を行います。<br>
+	 * ドライバーとの接続、プリペアの作成、ステートメントの作成、オートコミットの停止など
+	 */
+	public void initialize() throws AccessException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost/howdb", "root", "");
 			con.setAutoCommit(false);
 			write_pstmt = con.prepareStatement("Insert into socDB values(?,?,NOW())");
 			stmt = con.createStatement();
+			stmtRead = con.createStatement();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			throw new Exception("JDBCドライバのロードに失敗しました。");
+			throw new AccessException("JDBCドライバのロードに失敗しました。");
 		} catch (Exception e) {
 			e.printStackTrace();
-			throw new Exception("ドライバーでの失敗");
+			throw new AccessException("ドライバーでの失敗");
 		}
 	}
 
-	// 書き込み
-	public void write(Model model) throws SQLException {
+	/**
+	 * writeはDBに書き込みをします。<br>
+	 * 書き込む手段として、PKの番号は小さい順に総当りで探し書き込みを行います。<br>
+	 * コミットもこのメソッドで行います。失敗時にはロールバックをします。<br>
+	 * メソッドそのものをsynchronizedにしました。
+	 *
+	 * @param model
+	 *            Modelクラスの利用
+	 * @throws AccessException
+	 *             エラー時にメッセージをつけて処理
+	 * @see AutoExecuter
+	 */
+	public synchronized void write(Model model) throws AccessException {
 		try {
 			boolean flag = false;
+			nom = 1;
+			// synchronized (lock) {
 			while (true) {
-				sql = "exists(Select* from socDB where nom=" + nom + ")";
-				rset = stmt.executeQuery("Select " + sql);
-				while (rset.next()) {
-					if (rset.getString(sql).equals("0"))
-						flag = true;
-				}
+				sql = "Select* from socDB where nom=" + nom;
+				rset = stmt.executeQuery(sql);
+				if (!rset.next())
+					flag = true;
 				if (flag)
 					break;
 				nom++;
@@ -50,47 +91,58 @@ public class ClientDB {
 			write_pstmt.setInt(1, nom);
 			write_pstmt.setString(2, model.toString());
 			write_pstmt.executeUpdate();
+			con.commit();
+			// }
 		} catch (SQLException e) {
+			try {
+				e.printStackTrace();
+				con.rollback();
+			} catch (SQLException e1) {
+				throw new AccessException("ロールバックエラー");
+			}
 			e.printStackTrace();
-			throw new SQLException();
+			throw new AccessException("書き込みエラー");
 		}
 	}
 
-	// 読み込み
-	public Model read() throws SQLException {
-		sql = "Select* from socDB where upd_date=(Select Max(upd_date) from socDB)";
+	/**
+	 * readは最新更新日のレコードの読み込みをします。<br>
+	 * 読み込んだデータはModelクラスの形で帰します。<br>
+	 *
+	 * @return 見つけたレコードをModelクラスに引き渡す
+	 */
+	public Model read() throws AccessException {
+		sqlRead = "Select* from socDB where upd_date=(Select Max(upd_date) from socDB)";
 		try {
-			rset = stmt.executeQuery(sql);
-			while (rset.next())
-				sql = "キー値:" + nom + ",文章：" + rset.getString("message")
-						+ ",最終更新日：" + rset.getString("upd_date");
+			rsetRead = stmtRead.executeQuery(sqlRead);
+			while (rsetRead.next())
+				sqlRead = "キー値:" + rsetRead.getInt("nom") + ",文章：" + rsetRead.getString("message")
+						+ ",最終更新日：" + rsetRead.getString("upd_date");
 		} catch (SQLException e) {
 			e.printStackTrace();
-			throw new SQLException();
+			throw new AccessException("読み込みエラー");
 		}
-		return new Model(sql);
+		return new Model(sqlRead);
 	}
 
-	// クローズ
+	/** closeはクローズ処理を行います。 */
 	public void close() {
 		try {
-			if (con != null) {
-				con.commit();
+			if (con != null)
 				con.close();
-			}
 			if (stmt != null)
 				stmt.close();
+			if (stmtRead != null)
+				stmtRead.close();
 			if (write_pstmt != null)
 				write_pstmt.close();
 			if (rset != null)
 				rset.close();
+			if (rsetRead != null)
+				rsetRead.close();
 		} catch (SQLException e) {
-			try {
-				con.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
 			e.printStackTrace();
+			System.out.println("クローズ内でのエラー");
 		}
 	}
 }
--- takada/Model.java
+++ takada/Model.java
@@ -2,17 +2,32 @@ package socDB;
 
 import java.io.Serializable;
 
+/**
+ * Modelクラスはデータを格納するクラスです。<br>
+ * シリアライズ化しています。
+ *
+ */
 public class Model implements Serializable {
 	private final StringBuilder value;
 	// クラスのバージョンを識別する番号
 	private static final long serialVersionUID = 15654643L;
 
-	// コンストラクター
+	/**
+	 * 入力された文字をモデルへ格納します。
+	 *
+	 * @param value モデルクラスに格納する文字列
+	 */
 	public Model(String value) {
 		this.value = new StringBuilder(value);
 	}
 
-	// toString
+	/**
+	 * toStringはあらかじめコンパイルされるメソッドです。<br>
+	 * モデルクラスに格納した文字列をString型で返します。
+	 *
+	 * @return クラスに格納した文字列
+	 *
+	 */
 	@Override
 	public String toString() {
 		return value.toString();
